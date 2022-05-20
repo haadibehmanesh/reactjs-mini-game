d02be585042a74004a3c5d399610a49fc0f119b0
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-20 10:40:28 +0200

    add stage components to app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useContext } from "react";
+import Stage1 from "./components/stage_1";
+import Stage2 from "./components/stage_2";
+import { MyContext } from "./context";
+
 const App = () => {
-  return ( 
+
+  const context = useContext(MyContext);
+  console.log(context)
+  return (
     <>
+      <div className="wrapper">
+        <div className="center-wrapper">
+          <h1>Who pays the bill?</h1>
+          {
+            context.state.stage === 1 ?
+              <Stage1 />
+              :
+              <Stage2 />
+
+          }
+        </div>
+      </div>
+
+
     </>
-   );
+  );
 }
- 
+
 export default App;
