0d8ce5305c7003c58231ea97c0c6d51a1dddcaa4
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-23 20:04:10 +0200

    add toast react package and add this to my context to use in whole app

--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,6 @@
 import { Component, createContext } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MyContext = createContext();
 
@@ -16,14 +18,39 @@ class MyProvider extends Component {
         )
 
     }
+    removePlayerHandler = (index) => {
+        let newArray = this.state.players;
+        newArray.splice(index, 1);
+        this.setState({
+            players: newArray
+
+        })
+    }
+    nextHandler = () => {
+        const { players } = this.state;
+        if (players.length < 2) {
+            toast.error('You need more than one player!', {
+                position: toast.POSITION.TOP_LEFT
+            })
+        } else {
+            toast.success('moving to stage 2')
+        }
+
+    }
     render() {
         return (
-            <MyContext.Provider value={{
-                state: this.state,
-                addPlayer: this.addPlayerHandler
-            }}>
-                {this.props.children}
-            </MyContext.Provider>
+            <>
+                <MyContext.Provider value={{
+                    state: this.state,
+                    addPlayer: this.addPlayerHandler,
+                    removePlayer: this.removePlayerHandler,
+                    next: this.nextHandler
+                }}>
+                    {this.props.children}
+                </MyContext.Provider>
+                <ToastContainer />
+            </>
+
 
         );
     }
