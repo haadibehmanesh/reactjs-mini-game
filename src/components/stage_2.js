bf143375c1aaf65a6c8b2bae6bdd8a8e4b95dd6c
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-05-23 21:45:03 +0200

    add getLooser and start over functions to my context

--- src/components/stage_2.js
+++ src/components/stage_2.js
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import { MyContext } from "../context";
+
 const Stage2 = () => {
+    const context = useContext(MyContext);
+
     return (
         <>
-            Stage2
+            <div className="result_wrapper">
+                <h3>
+                    The Looser is :
+                </h3>
+                {context.state.result}
+            </div>
+
+            <div onClick={context.resetGame} className="action_button">Start Over</div>
+            <div onClick={context.getNewLooser} className="action_button btn_2">Get New Looser</div>
         </>
     );
 }
--- src/context/index.js
+++ src/context/index.js
@@ -33,9 +33,31 @@ class MyProvider extends Component {
                 position: toast.POSITION.TOP_LEFT
             })
         } else {
-            toast.success('moving to stage 2')
+
+            this.setState({
+                stage: 2
+            }, () => {
+                setTimeout(() => {
+                    this.generateLooser();
+                }, 1000)
+            })
+
         }
 
+    }
+    generateLooser = () => {
+        const { players } = this.state;
+        this.setState({
+
+            result: players[Math.floor(Math.random() * players.length)]
+        })
+    }
+    resetGame = () => {
+        this.setState({
+            stage: 1,
+            players: [],
+            result: ''
+        })
     }
     render() {
         return (
@@ -44,7 +66,9 @@ class MyProvider extends Component {
                     state: this.state,
                     addPlayer: this.addPlayerHandler,
                     removePlayer: this.removePlayerHandler,
-                    next: this.nextHandler
+                    next: this.nextHandler,
+                    getNewLooser: this.generateLooser,
+                    resetGame: this.resetGame
                 }}>
                     {this.props.children}
                 </MyContext.Provider>
